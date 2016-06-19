b30129b07f67aeeeb5dc62b85ae37d9d95e8ebbd
Lambros Petrou <lambrospetrou@gmail.com>
2016-06-19 13:58:52 +0100

    Added lists 02

--- sol/lists.go
+++ sol/lists.go
@@ -2,9 +2,18 @@ package sol
 
 import "errors"
 
+// P01 returns the last element of a list or error
 func P01(l []int) (int, error) {
 	if len(l) == 0 {
 		return -1, errors.New("No elements in list")
 	}
 	return l[len(l)-1], nil
+}
+
+// P02 returns the last two elements of a list or error
+func P02(l []int) ([]int, error) {
+	if len(l) < 2 {
+		return nil, errors.New("List has less than 2 elements")
+	}
+	return l[len(l)-2:], nil
 }
--- sol/lists_test.go
+++ sol/lists_test.go
@@ -1,6 +1,7 @@
 package sol_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/lambrospetrou/go99/sol"
@@ -13,6 +14,21 @@ func Test01(t *testing.T) {
 	}
 
 	if _, err := sol.P01([]int{}); err == nil {
-		t.Error("Expected err with empty list, got nil")
+		t.Error("Expected err, got nil err")
+	}
+}
+
+func Test02(t *testing.T) {
+	l := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 22, 33, 11, -9}
+	if res, _ := sol.P02(l); !reflect.DeepEqual([]int{11, -9}, res) {
+		t.Error("Expected [11, -9], got ", res)
+	}
+
+	if _, err := sol.P02([]int{1}); err == nil {
+		t.Error("Expected err with nil list, got nil err")
+	}
+
+	if _, err := sol.P02([]int{}); err == nil {
+		t.Error("Expected err with nil list, got nil err")
 	}
 }
